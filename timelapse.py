9f669524340479b2e652203ea4ca9f2258f3a1fc
Karsten Eger <github@mail.c3-net.de>
2020-11-24 15:15:20 +0100

    Update timelapse.py
    added run options and copy snap to extra folder to be used otherwise

--- timelapse.py
+++ timelapse.py
@@ -15,10 +15,11 @@ import picamera
 import sys
 import os
 import logging
+from shutil import copyfile
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-__output_folder_name__= sys.argv[3] + '/output'
+__output_folder_name__= sys.argv[5] + '/output'
 
 def clean_directory():
     os.system('rm -f '+__output_folder_name__+'/*.jpg')
@@ -30,11 +31,12 @@ def capture_images(length_in_seconds,interval_in_seconds):
         camera.start_preview()
         time.sleep(2)
         for filename in camera.capture_continuous(__output_folder_name__+'/img{counter:06d}.jpg'):
+            copyfile(filename, '/home/pi/camera/snap.jpg')
             time.sleep(interval_in_seconds) # wait <interval_in_seconds> seconds
             count -= 1
             if count <= 0:
                 break
-    
+
 # [START]
 def main():
     # Start with cleanup
@@ -50,13 +52,24 @@ def main():
     logging.info('Writing timestamps...')
 
     # Write timestamps to images
-    write_timestamps(72,32,__output_folder_name__)
-    logging.info('Captured images {} seconds to run'.format(str(time.time() - start_time)))
-
+    if int(sys.argv[3]) == 1:
+        logging.info('Writing timestamps and resizing images')
+        write_timestamps(72,32,__output_folder_name__)
+        logging.info('Captured images {} seconds to run'.format(str(time.time() - start_time)))
+    else:
+        logging.info('resizing and timestamps skipped')
     # Create video
-    logging.info('Making video...')
-    timestamp = str(datetime.now().strftime('%Y-%m-%d_%H%M%S'))
-    stitch_video('timelapse_'+timestamp+'.mp4','warning',__output_folder_name__,'video','img%06d-resized.jpg')
+    if int(sys.argv[4]) == 1:
+        logging.info('Making video...')
+        timestamp = str(datetime.now().strftime('%Y-%m-%d_%H%M%S'))
+        if int(sys.argv[3]) == 1:
+            logging.info('using resized images with timestamp')
+            stitch_video('timelapse_'+timestamp+'.mp4','warning',__output_folder_name__,'video','img%06d-resized.jpg')
+        else:
+            logging.info('using full size, no timestamp')
+            stitch_video('timelapse_'+timestamp+'.mp4','warning',__output_folder_name__,'video','img%06d.jpg')
+    else:
+        logging.info('video creation skipped')
     logging.info('Done!')
     logging.info('Creating video {} seconds to run'.format(str(time.time() - start_time)))
 if __name__ == '__main__':
